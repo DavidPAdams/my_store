1b6bbd51a77444538e0b71ea7288d9112280b6ca
David Adams <david61795@gmail.com>
2018-07-16 09:30:06 -0700

    start purchaseer

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'cart/add_to_cart'
+
+  get 'cart/view_order'
+
+  get 'cart/checkout'
+
   devise_for :users
   
   root 'storefront#all_products'
 
   get 'categorical' => 'storefront#by_category'
 
-  get 'branding' => 'storefront/by_brand'
+  get 'branding' => 'storefront#by_brand'
 
   resources :products
 end
